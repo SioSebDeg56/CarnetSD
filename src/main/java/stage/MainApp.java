f78d58b53db90b346e17f91520c4948947759635
Seb <sebastien.degasne@protonmail.com>
2022-05-28 13:38:58 +0200

    Projet SF ok

--- src/main/java/stage/MainApp.java
+++ src/main/java/stage/MainApp.java
@@ -32,7 +32,7 @@ public class MainApp extends Application {
     }
 
     public static void main(String[] args) {
-//test github
+//test github efzefezfezfzefezf vevezfezfezfezfezfef
         launch(args);
     }
 
